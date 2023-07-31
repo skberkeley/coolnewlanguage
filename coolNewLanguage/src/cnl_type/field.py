10254c54c5c0d945bbac081522039ef04145c1a9
Sora Kanosue <sorakanosue@berkeley.edu>
2023-07-31 15:46:38 -0700

    Added value and set_value method to field.py

--- coolNewLanguage/src/cnl_type/field.py
+++ coolNewLanguage/src/cnl_type/field.py
@@ -1,3 +1,8 @@
+from typing import Any
+
+from coolNewLanguage.src.exceptions.CNLError import raise_type_casting_error
+
+
 class Field:
     """
     A field within a programmer-defined CNLType
@@ -5,8 +10,10 @@ class Field:
     Attributes:
         data_type: The expected type of data that will live in this Field
         optional: Whether this Field is required for every instance of the host CNLType
+        value: The actual value present in this field in a particular CNLType instance. Objects setting this value
+            use the set_value method to ensure that values are properly type cast
     """
-    __slots__ = ('data_type', 'optional')
+    __slots__ = ('data_type', 'optional', 'value')
 
     def __init__(self, data_type: type, optional: bool = False) -> None:
         if not isinstance(data_type, type):
@@ -16,3 +23,16 @@ class Field:
 
         self.data_type = data_type
         self.optional = optional
+        self.value = None
+
+    def set_value(self, value: Any) -> None:
+        """
+        Casts val to this field's data_type before setting its value.
+        :param value:
+        :return:
+        """
+        try:
+            value = self.data_type(value)
+        except Exception as e:
+            raise_type_casting_error(value, self.data_type, e)
+        self.value = value
