fa0cdcba2b8d2b2654bb4d9932bae612021f3af8
Sora Kanosue <sorakanosue@berkeley.edu>
2024-08-09 17:42:45 +0200

    Make approval batch operations into normal buttons

--- coolNewLanguage/web/static/approvals.js
+++ coolNewLanguage/web/static/approvals.js
@@ -1,25 +1,20 @@
-function handleRadioControl() {
-    const approveAllRadio = document.getElementById('approveAll');
-    const rejectAllRadio = document.getElementById('rejectAll');
-    const manualRadio = document.getElementById('manualSelection');
+function approveAll() {
+    const allApproveRadios = document.querySelectorAll(
+        'input[type="radio"][value="approve"]'
+    );
+    allApproveRadios.forEach(radio => { radio.checked = true });
+}
 
-    if (approveAllRadio.checked) {
-        // Shift all radio buttons to approve
-        const allApproveRadios = document.querySelectorAll(
-            'input[type="radio"][value="approve"]'
-        );
-        allApproveRadios.forEach(radio => { radio.checked = true });
-    } else if (rejectAllRadio.checked) {
-        // Shift all radio buttons to reject
-        const allRejectRadios = document.querySelectorAll(
-            'input[type="radio"][value="reject"]'
-        );
-        allRejectRadios.forEach(radio => { radio.checked = true });
-    } else if (manualRadio.checked) {
-        // Shift all radio buttons to pending
-        const allPendingRadios = document.querySelectorAll(
-            'input[type="radio"][value="pending"]'
-        );
-        allPendingRadios.forEach(radio => { radio.checked = true });
-    }
+function rejectAll() {
+    const allRejectRadios = document.querySelectorAll(
+        'input[type="radio"][value="reject"]'
+    );
+    allRejectRadios.forEach(radio => { radio.checked = true });
+}
+
+function pendAll() {
+    const allPendingRadios = document.querySelectorAll(
+        'input[type="radio"][value="pending"]'
+    );
+    allPendingRadios.forEach(radio => { radio.checked = true });
 }
